708190f47be298ba0af45a347e5fd11723674df7
hasan <hasanocal7@gmail.com>
2023-11-25 16:10:30 +0300

    Lecture Notes and My Works

---
+++ Hafta6/vueproject/vuetify-project/src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-text-field
+        v-model="name"
+        :label="nameLabel"
+        :variant="variant"
+      ></v-text-field>
+      <v-btn @click="sendName(name)">Send Name</v-btn>
+      <h3 :style="{ color: activeColor, fontSize: fontSize + 'px' }">
+        {{ name }}
+      </h3>
+      <h3>{{ fullName }}</h3>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      midName: "",
+      surName: "Öcal",
+      nameLabel: "Name",
+      variant: "outlined",
+      activeColor: "green",
+      fontSize: 25,
+    };
+  },
+  computed: {
+    fullName() {
+      return this.name + this.midName + this.surName;
+    },
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {
+    sendName(val) {
+      console.log(this.nameLabel);
+      this.alertBig(val);
+    },
+    alertBig(val) {
+      alert(val);
+    },
+  },
+  watch: {
+    name(val, oldVal) {
+      console.log("val: " + val, "oldVal: " + oldVal);
+    },
+  },
+};
+</script>
